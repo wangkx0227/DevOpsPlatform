d4f7b248e49fbc76b65fff145b083dd63de8698a
it-21@1bzimail.com <it-21@1bzimial.com>
2025-02-12 11:23:07 +0800

    '更新'

--- ssh.py
+++ ssh.py
@@ -2,20 +2,17 @@ import paramiko  # https://docs.paramiko.org/en/stable/ 文档，实现ssh链接
 import logging
 
 # 配置日志
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',filename='paramiko.log', filemode='w')
-
-
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#                     filename='paramiko.log', filemode='w')
 
 ssh = paramiko.SSHClient()  # 创建SSH客户端
 
-ssh.set_log_channel("测试")
+# ssh.set_log_channel("测试")
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 设置信任远程机器，允许连接不在known_hosts文件中的主机
 
-
-
-ssh.connect(hostname='10.8.10.8', port=8000, username='cjl',password='123') # 连接到远程服务器
-
-
+ssh.connect(hostname='10.8.10.8', port=8000, username='cjl', password='123')  # 连接到远程服务器
+stdin, stdout, stderr = ssh.exec_command("free -h")  # 执行命令，并且返回错误信息正确信息
+result = stdout.read() + stderr.read()
+print(result.decode())
 ssh.close()
-
